e00ac0743d55bc831531437f2e1b3bdd45f19075
DEVMONTANNA <chiedoziegoodluck351@gmail.com>
2025-10-21 15:34:38 +0100

    new

--- src/Component/BrowseDapps.jsx
+++ src/Component/BrowseDapps.jsx
@@ -7,7 +7,7 @@ const BrowseDapps = () => {
      <div className="bg-gradient-to-b lg:w-[92%] rounded-[30px] from-[#fff9a8] via-[#ffaefe] to-[#ffaefe] flex flex-col justify-center items-center p-[25px] ">  
       <div className='flex flex-col lg:items-center lg:justify-center '>
         <p className='lg:text-[3.2rem] md:text-[2rem] text-[2rem] font-semibold translate-2'>Browse a world of dApps</p>
-     <p className=' text-[1.1rem]  mt-[20px]'> Access Web3 and DeFi opportunities via our dApp browser</p>
+     <p className=' text-[1.1rem] font-stretch-extra-condensed mt-[20px]'> Access Web3 and DeFi opportunities via our dApp browser</p>
      </div>
 
        <div className=' bg-[white] w-full lg:w-[85%] flex flex-col lg:flex-row items-center justify-between rounded-[30px] p-[10px] lg:p-[20px] mb-6 lg:mb-[30px] mx-auto'>
@@ -15,7 +15,7 @@ const BrowseDapps = () => {
     <p  className='text-[25px] text-black  font-bold '>Deposit crypto easily from exchanges</p>
     <p  className='text-[16px] text-black mt-[15px] '>Take control of your crypto. Avoid complicated steps and deposit directly to your wallet from exchanges like Binance and Coinbase.</p>
     <Button
-  className="border border-[blue] lg:w-[] w-[100%] hover:bg-[blue] hover:text-[white] cursor-pointer text-black p-2 rounded-[23px]  mt-[20px] justify-center flex items-center gap-3"
+  className="border border-[blue] hover:bg-[blue] hover:text-[white] cursor-pointer text-black p-2 rounded-[23px]  mt-[20px] flex items-center gap-3"
   textValue="Get started with deposits"
   textClassName="text-[16px] font-medium"
 />
