bd2ae3433b105255ac544e21078094a71c5f304a
DEVMONTANNA <chiedoziegoodluck351@gmail.com>
2025-10-29 15:19:35 +0100

    new

--- src/Component/BrowseDapps.jsx
+++ src/Component/BrowseDapps.jsx
@@ -50,7 +50,7 @@ const BrowseDapps = () => {
       </div>
 
       <div className='w-full px-5 sm:px-6 md:px-8 lg:px-4 pb-8 md:pb-12 flex items-center justify-center -mt-20 lg:-mt-24'>
-        <div className='z-[-10] lg:bg-gradient-to-b from-[#fff465] via-[#48ff91] to-[#fff465] bg-[#fff465] w-full lg:w-[92%] max-w-7xl rounded-3xl lg:rounded-[30px] flex flex-col justify-center items-center px-4 sm:px-6 md:px-8 lg:px-12 pt-24 lg:pt-28 pb-8 md:pb-12 lg:pb-16'>
+        <div className='z-[-10] lg:bg-gradient-to-b from-[#fff465] via-[#48ff91] to-[#48ff91] bg-[#fff465] w-full lg:w-[92%] max-w-7xl rounded-3xl lg:rounded-[30px] flex flex-col justify-center items-center px-4 sm:px-6 md:px-8 lg:px-12 pt-24 lg:pt-28 pb-8 md:pb-12 lg:pb-16'>
             <div className='flex flex-col w-full text-center lg:text-center px-2 sm:px-4'>
             <p className='text-3xl font-[verdana] sm:text-4xl md:text-5xl lg:text-6xl xl:text-[3.2rem] font-semibold leading-tight sm:leading-tight md:leading-tight'>
              Zero personal tracking           
@@ -98,13 +98,13 @@ const BrowseDapps = () => {
  <div className='bg-white lg:w-[30%] w-full flex flex-col items-center justify-start p-4 lg:h-[350px] rounded-[25px]'>
     <p className='text-[20px] font-[verdana] mb-4'>Zero personal tracking</p>
     <img src={Security} alt="padlock icon" className='w-[50%] max-w-[120px] mt-[30px]'/>
-    <p className=' mt-[75px]'>Use our Encrypted Cloud Backup for increased wallet security.</p>
+    <p className=' mt-[75px]'>We don't track any personal information, including your IP address or balances.</p>
   </div>
 
   <div className='bg-white lg:w-[30%] w-full flex flex-col items-center justify-start p-4 lg:h-[350px] rounded-[25px]'>
     <p className='text-[20px] font-[verdana] mb-4'>Proactive alerts for risky transactions</p>
     <img src={Tracking} alt="padlock icon" className='w-[33%] max-w-[120px] mt-[30px]'/>
-    <p className=' mt-[45px]'>Use our Encrypted Cloud Backup for increased wallet security.</p>
+    <p className=' mt-[45px]'>Stay safe with alerts for risky address and dApp connections.</p>
   </div>
 
 </div>
